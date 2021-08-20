613f08264d5f048a35abaf13f1039e198dbacba7
Yamaguchi-shuhei <81962317+Yamaguchi-shuhei@users.noreply.github.com>
2021-08-21 04:50:20 +0900

    ver.1.6.6(ドア完成, 状況の表示完成)

--- Assets/Scripts/Sequences/AOpening.cs
+++ Assets/Scripts/Sequences/AOpening.cs
@@ -20,7 +20,7 @@ public class AOpening : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         FadeScreenIn.SetActive(false);
-        TextBox.GetComponent<Text>().text = "nγじょvsんjんfbjsrn";
+        TextBox.GetComponent<Text>().text = "I need to get out ";
 
         yield return new WaitForSeconds(2);
         TextBox.GetComponent<Text>().text = "";
